d66280a4f57299d2c939b81e5ae4665796d6d883
profanis <fanis@projectbeagle.com>
2018-12-05 15:12:45 +0200

    mongo config for differenct environments

--- config/mongo.config.ts
+++ config/mongo.config.ts
@@ -6,8 +6,14 @@ export class MongoDbConnetion {
     }
 
     public initialize() {
-        const uri = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
-        mongoose.set("debug", true);
+        let uri: string;
+        console.log(process.env.NODE_ENV);
+        if (process.env.NODE_ENV === "development") {
+            uri = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+            mongoose.set("debug", true);
+        } else {
+            uri = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+        }
         const mongooseInstance = mongoose.connect(uri, {}, (err: any) => {
             if (err) {
                 console.log(err);
