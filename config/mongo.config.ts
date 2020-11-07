928743786e81bbc42a57846459f01922ea3bd36a
Fanis Prodromou <fanis.prodromou@architechts.gr>
2020-11-07 14:54:21 +0200

    mondodb atlas changes

--- config/mongo.config.ts
+++ config/mongo.config.ts
@@ -8,16 +8,16 @@ export class MongoDbConnetion {
     public initialize() {
         let uri: string;
         if (process.env.NODE_ENV === "development") {
-            uri = `mongodb://${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+            uri = `mongodb://${process.env.DB_HOST}/${process.env.DB_NAME}`;
             mongoose.set("debug", true);
         } else {
-            uri = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`;
+            uri = `mongodb://${process.env.DB_USERNAME}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}/${process.env.DB_NAME}`;
         }
         const mongooseInstance = mongoose.connect(uri, {}, (err: any) => {
             if (err) {
                 console.log(err);
             } else {
-                console.log(`mongodb connection is succesfully establised in port ${process.env.DB_PORT}!`);
+                console.log(`mongodb connection is successfully established!`);
             }
         });
     }
